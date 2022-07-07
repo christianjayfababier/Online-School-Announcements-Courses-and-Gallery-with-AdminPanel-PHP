95071b7bc050e5c45154a08d4f4f4122290394d8
kaiGen_dev <38854513+christianjayfababier@users.noreply.github.com>
2022-07-07 13:06:58 +0800

    Create index.php
    School landing page

---
+++ index.php
@@ -0,0 +1,101 @@
+<!-- HTML header-->
+<?php include('components/src.php');?>
+<!-- HTML header-->
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <?php include('components/navigation.php');?>
+
+        
+        <!-- PRMSU Dymanic header-->
+        <header>
+          
+            <?php include('components/header.php');?>
+            
+        </header>
+
+
+        <!-- QPRMSU About Seciton-->
+        <aside class="text-center">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-xl-8">
+                    <img src="assets/img/prmsu.png" alt="..." style="height: 10rem; margin-bottom:15px" />
+                        <div class="h2 fs-1 mb-4">About PRMSU</div>
+                        
+                        <p>President Ramon Magsaysay State University, commonly referred to as PRMSU, is a state university in Zambales established in 1910. The university is named after the province's greatest son and former President of the Republic of the Philippines, President Ramon Magsaysay. In 2017, the name of the university was changed from Ramon Magsaysay Technological University to its present name. The main campus of the university is located in the Municipality of Iba, Province of Zambales. The President Ramon Magsaysay State University, a merger of three public education institutions in the province of Zambales, was established by virtue of Republic Act 8498, enacted on February 12, 1998, through the initiative of Congressman Antonio M. Diaz.</p>
+                       
+                    </div>
+                </div>
+            </div>
+        </aside>
+        
+
+         <!-- PRMSU Student Admission Section-->
+
+        <section class="bg-light">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+                    
+                <div class="col-12 col-lg-6">
+                    <p style="color:#FFCF00" class="justify-content-center">PRESIDENT RAMON MAGSAYSAY STATE UNIVERSITY</p>    
+                    <div class="h2 fs-1 mb-4">Student Admission</div>
+                        <br>
+                    <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+                        <br>
+                    <a class="btn btn-outline-dark py-3 px-4 rounded-pill" href="" target="_blank">Learn More</a>
+
+                    </div>
+                    <div class="col-sm-8 col-lg-6 col-md-6">
+                        <div class="px-5 px-sm-0"><img class="img-fluid rounded-circle" src="assets/img/admission.jpg" alt="..." /></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Mission Vision section-->
+        <section id="features" style="background: rgb(17,10,134);
+            background: linear-gradient(90deg, rgba(17,10,134,1) 0%, rgba(9,9,121,1) 28%, rgba(18,105,175,1) 100%);">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-12 order-lg-1 mb-5 mb-lg-0">
+                        <div class="container-fluid px-5">
+                            <div class="row gx-5">
+                                <div class="col-md-6 mb-5">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-flag icon-feature text-white d-block mb-3"></i>
+                                        <h3 class="font-alt text-white">Mission</h3>
+                                        <p class="mb-0 text-white">President Ramon Magsaysay State University shall primarily provide advance and higher professional, technical, and special instructions in various disciplines; undertake research, extension and income generation programs for the sustainable development of Zambales, the region and the country.</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mb-5">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-eye icon-feature text-white d-block mb-3"></i>
+                                        <h3 class="font-alt text-white">Vision</h3>
+                                        <p class="mb-0 text-white">The President Ramon Magsaysay State University shall be a premier learner-centered and proactive university in a digital and global society.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </section>
+
+
+      
+        <!-- Footer-->
+        <?php include('components/footer.php');?>
+       
+        <!-- Bootstrap core JS-->
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+      
+        <script src="assets/js/sb-forms-latest.js"></script>
+    </body>
+</html>
