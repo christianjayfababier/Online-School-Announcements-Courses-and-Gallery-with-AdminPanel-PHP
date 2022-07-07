08be733c24307488e9990af3e47225ac87c2d81d
kaiGen_dev <38854513+christianjayfababier@users.noreply.github.com>
2022-07-07 13:25:26 +0800

    Create header.php

---
+++ components/header.php
@@ -0,0 +1,62 @@
+<?php include('src.php');?>
+<?php
+  $link1 = '';
+  $link2 = 'announcements';
+  $link3 = 'courses';
+  $link4 = 'gallery';
+
+?>
+
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel" >
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+    </div>
+    <div class="carousel-inner">
+
+    <div class="carousel-item active" >
+        <img src="assets/img/slide-03.jpg" class="d-block w-100" alt="" >
+
+
+        <div class="container">
+          <div class="carousel-caption">
+            <h1><span style="color:#FFCF00"> President Ramon Magsaysay State University </span> - San Marcelino Campus</h1>
+            <p>Keep up with the most recent announcements and events.</p>
+            <p><a class="btn btn-lg btn-warning"  href="http://localhost/prmsusm.edu.ph/<?= $link2 ?>/">View Announcements</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <img src="assets/img/slide-02.jpg" class="d-block w-100" alt="">
+
+        <div class="container">
+          <div class="carousel-caption">
+          <h1><span style="color:#FFCF00"> President Ramon Magsaysay State University </span> - San Marcelino Campus</h1>
+            <p>Check out our available courses.</p>
+            <p><a class="btn btn-lg btn-warning"   href="http://localhost/prmsusm.edu.ph/<?= $link3 ?>/">View All Courses</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <img src="assets/img/slide-01.jpg" class="d-block w-100" alt="">
+
+    <div class="container">
+    <div class="carousel-caption">
+    <h1><span style="color:#FFCF00"> President Ramon Magsaysay State University </span> - San Marcelino Campus</h1>
+        <p>Check out our campus virtual tour</p>
+        <p><a class="btn btn-lg btn-warning"  href="http://localhost/prmsusm.edu.ph/<?= $link4 ?>/">Start Tour</a></p>
+    </div>
+    </div>
+</div>
+
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
