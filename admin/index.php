a99da798ae7941279d8a254dc294e2832ec81d0a
kaiGen_dev <38854513+christianjayfababier@users.noreply.github.com>
2022-07-07 13:34:40 +0800

    Create Admin Index.php

---
+++ admin/index.php
@@ -0,0 +1,163 @@
+<html lang="en"><script async="false" type="text/javascript" src="chrome-extension://fnjhmkhhmkbjkkabndcnnogagogbneec/in-page.js"></script><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>Login</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
+    <script src="assets/n-login/jquery.min.js"></script>
+
+  
+    
+    <link href="assets/n-login/bootstrap.min.css" rel="stylesheet" >
+<link href="bootstrap.min.css" rel="stylesheet" >
+
+    <!-- Favicons -->
+<link rel="icon" href="assets/img/prmsu.png">
+<meta name="theme-color" content="#712cf9">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sign-in.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+<main class="form-signin w-100 m-auto">
+  <form method="post" name="login_form">
+    <img class="mb-4" src="../assets/img/prmsu.png" alt="" width="72" height="auto">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="text" class="form-control" id="username" placeholder="Username" alt="username" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+      <label for="floatingInput">Username</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="password" alt="password" placeholder="Password" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit"  value="Sign in" id="btn-login" name="btn-login">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">PRMSU © 2022</p>
+  </form>
+</main>
+
+
+    
+<script src="assets/n-login/bootstrap.bundle.min.js" ></script>
+<script src="assets/n-login/popper.min.js"></script>
+<script src="assets/n-login/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+      document.oncontextmenu = document.body.oncontextmenu = function() {return false;}//disable right click
+    </script>
+  
+     <script type="text/javascript">
+            jQuery(function(){
+                $('form[name="login_form"]').on('submit', function(e){
+                    e.preventDefault();
+
+                    var u_username = $(this).find('input[alt="username"]').val();
+                    var p_password = $(this).find('input[alt="password"]').val();
+                   // var s_status = 1;
+
+                    if (u_username === '' && p_password ===''){
+                        $('#alert-msg').html('<div class="alert alert-danger"> Required Username and Password!</div>');
+                    }else{
+                        $.ajax({
+                            type: 'POST',
+                            url: 'init/controllers/login.php',
+                            data: {
+                                username: u_username,
+                                password: p_password
+                               // status: s_status
+                            },
+                            beforeSend:  function(){
+                                $('#alert-msg').html('');
+                            }
+                        })
+                        .done(function(t){
+                            if (t == 0){
+                                $('#alert-msg').html('<div class="alert alert-danger">Incorrect username or password!</div>');
+                                
+                            }else{
+                                $("#btn-login").html('<img src="assets/login/images/loading.gif" /> &nbsp; Signing In ...');
+                                setTimeout(' window.location.href = "routes/index.php"; ',2000);
+                            }
+                        });
+                    }
+                });
+           });
+        </script>
+        <script>
+            function myFunction() {
+              var x = document.getElementById("password");
+              if (x.type === "password") {
+                x.type = "text";
+              } else {
+                x.type = "password";
+              }
+            }
+     </script>
+
+</body></html>
